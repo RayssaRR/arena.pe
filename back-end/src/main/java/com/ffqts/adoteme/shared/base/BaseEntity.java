9469b1e7698c97072eb9bf1a84d480853c9e6b50
RayssaRR <rayssa_snt@hotmail.com>
2026-04-02 20:13:43 -0300

    create front-end

--- back-end/src/main/java/com/ffqts/adoteme/shared/base/BaseEntity.java
+++ back-end/src/main/java/com/ffqts/adoteme/shared/base/BaseEntity.java
@@ -6,17 +6,17 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 public abstract class BaseEntity {
 
-    @Column(updatable = false)
+    @Column(updatable = false)//como se comporta no banco
     private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
 
-    @PrePersist
+    @PrePersist//vai ser exeutado automaticamente antes do objeto ser salvo automaticamente no banco
     public void prePersist() {
         this.createdAt = LocalDateTime.now();
     }
 
-    @PreUpdate
+    @PreUpdate//ajusta a data da última modificação
     public void preUpdate() {
         this.updatedAt = LocalDateTime.now();
     }
