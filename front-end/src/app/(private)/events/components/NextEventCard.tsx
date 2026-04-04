12c5e5de610bca3245a45b5e1a50b4a0db17bb64
Raiele Leite <raiele.leite@hotmail.com>
2026-04-04 20:03:12 -0300

    add and list events without API

---
+++ front-end/src/app/(private)/events/components/NextEventCard.tsx
@@ -0,0 +1,53 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  CardContent
+} from "@/components/ui/card";
+
+export default function NextEventCard() {
+  return (
+    <Card className="mx-auto w-full pt-0">
+      {/* Header (título + categoria) */}
+      <CardHeader className="bg-black/70 text-white p-5">
+        <p className="bg-white p-1 rounded-md text-blue-600 font-bold w-fit px-4">
+          Esporte
+        </p>
+
+        <CardTitle className="text-xl mt-4">
+          Santa Cruz vs. Figueirense
+        </CardTitle>
+
+        <CardDescription className="text-white">
+          Acompanhe a partida válida pela Série C 2026.
+        </CardDescription>
+      </CardHeader>
+
+      {/* Conteúdo */}
+      <CardContent className="space-y-2 p-5">
+        <p className="text-blue-600 font-bold">OUT 11 - 16:30</p>
+
+        <h4 className="body-lg font-semibold">
+          Informações do assento
+        </h4>
+
+        <p className="text-gray-500 text-sm">
+          Arquibancada Setor Sul.
+        </p>
+      </CardContent>
+
+      {/* Footer */}
+      <CardFooter className="flex justify-end gap-5 bg-white p-5">
+        <Button className="bg-red-600 hover:bg-red-700">
+          Cancelar
+        </Button>
+        <Button className="bg-blue-600 hover:bg-blue-700">
+          Obter ingresso
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
---
+++ front-end/src/app/(public)/page.tsx
@@ -0,0 +1,10 @@
+import EventForm from "../(private)/events/EventForm";
+import MyEvents from "../(private)/events/MyEvents";
+
+export default function Page() {
+  return (
+    <main className="m-5">
+      <EventForm/>
+    </main>
+  );
+}
